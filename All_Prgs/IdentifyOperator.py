89dad7e5b7568dff7f801957089332710dc08823
vardhan65 <vardhanadheli1527@gmail.com>
2021-08-14 20:45:16 +0530

    operator

---
+++ All_Prgs/IdentifyOperator.py
@@ -0,0 +1,16 @@
+#Identify Operator
+
+
+x1=5
+x2="Hello"
+x3=[1,2,3]
+
+y1=5
+y2="Hello"
+y3=[1,2,3]
+
+
+print("x1 is not y2",x1 is not y2)
+print("x1 is not y1",x1 is not y1)
+print("x2 is y2",x2 is y2)
+
