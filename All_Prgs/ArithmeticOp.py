e0a55cd68975bbd535812092fbbec3a74786c786
vardhan65 <vardhanadheli1527@gmail.com>
2021-08-14 20:29:52 +0530

    DataTypes

---
+++ All_Prgs/ArithmeticOp.py
@@ -0,0 +1,17 @@
+x=20
+y=10
+
+print("x=",x)
+print("y=",y)
+
+print("\n 20 + 10 =",x+y)
+
+print("\n 20 - 10=",x-y)
+
+print("\n 20 * 10=",x*y)
+
+print("\n 20 - 10=",x/y)
+
+print("\n 20**10=",pow(x,y))
+
+print("\n 20  % 10=",x%y)
