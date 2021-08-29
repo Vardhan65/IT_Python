5572597b77871ef86aae16baac60149f0492afec
Vardhan65 <vardhanadheli1527@gmail.com>
2021-08-29 12:27:14 +0530

    Program

---
+++ All_Prgs/Assigments_2/Q2.py
@@ -0,0 +1,22 @@
+# Write a program to generate employee ID-Card(image not required,id card show on console) 
+
+
+name=str(input(" Enter your name ="))
+cname=str(input(" Enter your  company name ="))
+mobile=str(input(" Enter your mobile number ="))
+email=str(input(" Enter your email id ="))
+address=str(input(" Enter your address ="))
+blood=str(input(" Enter your blood group = "))
+
+
+print("-------------------ID Card----------------")
+
+
+print("Name :-  ",name)
+print("company name=",cname)
+print("Mobile No :- ",mobile)
+print("Email ID :- ",email)
+print ("Address :- ",address)
+print("Blood Group :- ",blood)
+
+print("-----------------------------------------")
